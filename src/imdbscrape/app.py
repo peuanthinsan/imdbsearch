db15b79df7e86bd4d16c27703910bca59047fcee
peuanthinsan <heartpickle@gmail.com>
2025-07-01 00:22:22 +0700

    Use Flask server instead of uWSGI

--- src/imdbscrape/app.py
+++ src/imdbscrape/app.py
@@ -1,19 +1,21 @@
-from urllib.parse import parse_qs
-
+from flask import Flask, request, Response
 from imdbscrape.peopleandmovies import PeopleAndMovies
 
+app = Flask(__name__)
+
 imdbsearch = PeopleAndMovies("/search/title", 20, {
-	"groups": "top_1000",
-	"sort": "user_rating",
-	"view": "simple",
+    "groups": "top_1000",
+    "sort": "user_rating",
+    "view": "simple",
 })
 
-def application(env, start_response):
-	response = "Indexing..."
-	start_response('200 OK', [('Content-Type','application/json')])
+@app.route('/', methods=['GET'])
+def search():
+    response = "Indexing..."
+    name = request.args.get('name')
+    if name:
+        response = imdbsearch.search(name)
+    return Response(str(response), mimetype='application/json')
 
-	query_string = parse_qs(env['QUERY_STRING'])
-	if 'name' in query_string and len(query_string['name']):
-		response = imdbsearch.search(query_string['name'][0])
-
-	return str(response).encode()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
